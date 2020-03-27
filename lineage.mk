9b46f1acf6b43bfe71df3bb6d02e3e1d651d8993
7spoons <desiato1978@gmail.com>
2020-03-27 22:57:48 +0000

    rebranding cm to lineage

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Inherit device configuration for mt8127-common
-$(call inherit-product, device/amazon/mt8127-common/cm.mk)
+$(call inherit-product, device/amazon/mt8127-common/lineage.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/amazon/ford/full_ford.mk)
@@ -9,5 +9,5 @@ PRODUCT_CHARACTERISTICS := tablet
 TARGET_SCREEN_WIDTH := 600
 TARGET_SCREEN_HEIGHT := 1024
 
-PRODUCT_NAME := cm_ford
+PRODUCT_NAME := lineage_ford
 PRODUCT_RELEASE_NAME := ford
